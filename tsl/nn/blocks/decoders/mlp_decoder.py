2edf576f7ff33f8ba5c86f184493cb4b306f0a47
andreacini <andrea.cini@usi.ch>
2022-03-17 11:17:30 +0100

    more flexible MLP decoder

--- tsl/nn/blocks/decoders/mlp_decoder.py
+++ tsl/nn/blocks/decoders/mlp_decoder.py
@@ -3,13 +3,15 @@ from torch import nn
 from tsl.nn.blocks.encoders.mlp import MLP
 from einops.layers.torch import Rearrange
 
+from einops import rearrange
+
 
 class MLPDecoder(nn.Module):
     r"""
     Simple MLP decoder for multi-step forecasting.
 
-    If the input representation has a temporal dimension, this model will simply take the representation corresponding
-    to the last step.
+    If the input representation has a temporal dimension, this model will take the flatten representations corresponding
+    to the last `receptive_field` time steps.
 
     Args:
         input_size (int): Input size.
@@ -17,6 +19,7 @@ class MLPDecoder(nn.Module):
         output_size (int): Output size.
         horizon (int): Output steps.
         n_layers (int, optional): Number of layers in the decoder. (default: 1)
+        receptive_field (int, optional): Number of steps to consider for decoding. (default: 1)
         activation (str, optional): Activation function to use.
         dropout (float, optional): Dropout probability applied in the hidden layers.
     """
@@ -26,12 +29,14 @@ class MLPDecoder(nn.Module):
                  output_size,
                  horizon=1,
                  n_layers=1,
+                 receptive_field=1,
                  activation='relu',
                  dropout=0.):
         super(MLPDecoder, self).__init__()
 
+        self.receptive_field = receptive_field
         self.readout = nn.Sequential(
-            MLP(input_size=input_size,
+            MLP(input_size=receptive_field * input_size,
                 hidden_size=hidden_size,
                 output_size=output_size * horizon,
                 n_layers=n_layers,
@@ -44,5 +49,7 @@ class MLPDecoder(nn.Module):
         # h: [batches (steps) nodes features]
         if h.dim() == 4:
             # take last step representation
-            h = h[:, -1]
+            h = rearrange(h[:, -self.receptive_field:], 'b s n c -> b n (s c)')
+        else:
+            assert self.receptive_field == 1
         return self.readout(h)
