92bbf2c9c2dcb5d9b9b8a79239b94967002dfb18
arahosu <arahosu@gmail.com>
2022-05-16 12:55:29 +0200

    commenting out assert statement in base_predictor.py

--- tsl/nn/base/attention/attention.py
+++ tsl/nn/base/attention/attention.py
@@ -141,3 +141,59 @@ class MultiHeadAttention(nn.MultiheadAttention):
             attn_weights = rearrange(attn_weights, '(b d) l m -> b d l m',
                                      b=batch).contiguous()
         return attn_output, attn_weights
+
+
+class GraphormerMHA(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 embed_dim,
+                 num_heads):
+        super(GraphormerMHA, self).__init__()
+        
+        assert embed_dim % num_heads == 0, "Embedding dimension must be evenly divisible by the number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        # Stack all weight matrices 1...h together for efficiency
+        self.qkv_proj = nn.Linear(input_dim, 3*embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def scaled_dot_product(self, q, k, v, mask=None):
+        d_k = q.size()[-1]
+        attn_logits = torch.matmul(q, k.transpose(-2, -1))
+        attn_logits = attn_logits / math.sqrt(d_k)
+        if mask is not None:
+            attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+        attention = F.softmax(attn_logits, dim=-1)
+        values = torch.matmul(attention, v)
+        return values, attention
+    
+    def forward(self, x, mask=None, return_attention=True):
+        batch_size, seq_length, embed_dim = x.size()
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3*self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = self.scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
